d88e456c0434bc34472fa865895ba1d5ba3f825f
Emma-Theresa <emmatheresajose12.4@gmail.com>
2024-07-25 19:43:32 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, SimpleChange, ɵɵNgOnChangesFeature } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RoomComponent } from "./room/room.component";
+import { BedroomComponent } from "./bedroom/bedroom.component";
+import { KitchenComponent } from "./kitchen/kitchen.component";
+import { ButtonComponent } from "./button/button.component";
+import { CardsComponent } from "./Ui/cards/cards.component";
+
+import { CardListComponent } from "./Ui/card-list/card-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RoomComponent, BedroomComponent, KitchenComponent, ButtonComponent, CardsComponent, CardListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent{
+  title = 'prodcut-app'
 }
+
